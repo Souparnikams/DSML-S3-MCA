f35abc3f12eba05b3d8ca033834c96ec59cfe722
Souparnikams <116794014+Souparnikams@users.noreply.github.com>
2023-10-09 14:21:01 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,7 @@
+import mathplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[1,4,9,16,25]
+plt.plot(x,y)
+plt.xlabel("Red")
+plt.ylabel("Square")
+plt.show()
---
+++ typevar.py
@@ -0,0 +1,14 @@
+f=1.35
+print(f)
+print(type(f))
+s1="python"
+s2="world"
+s3='%s %s %d' %(s1,s2,199)
+print(s3)
+print(len(s1))
+print(s1.upper())
+print(s3.split())
+s1_r=s1[:: -1]
+print(s1_r)
+person={'name':'Souparnika','age':'21'}
+print(person['name'])
